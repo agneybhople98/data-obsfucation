10aa8a21a1acb856705555d270ffc3dd3acd66da
Agney Arun Bhople <agney@Agneys-MacBook-Pro.local>
2025-04-30 16:00:02 +0530

    restore timer

--- src/app/pages/restore/restore.component.ts
+++ src/app/pages/restore/restore.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { JobsDataService } from '../../jobs-data.service';
+import { interval, Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-restore',
@@ -8,12 +11,43 @@ import { JobsDataService } from '../../jobs-data.service';
   standalone: false,
 })
 export class RestoreComponent implements OnInit {
-  constructor(private _jobService: JobsDataService) {}
+  isRestoring: boolean = true;
+  countdown: number = 5;
+  timerSubscription: Subscription | null = null;
+
+  constructor(private _jobService: JobsDataService, private router: Router) {}
+
   ngOnInit(): void {
     this._jobService
       .runJobByName('OBF_REVERT_CHANGES_ARCHDEV_JOB')
       .subscribe((res) => {
         console.log(res);
+        this.isRestoring = false;
       });
+    this.startCountdown();
+  }
+
+  startCountdown(): void {
+    // Start with a 5 second countdown
+    this.countdown = 5;
+
+    // Create an interval that emits every second
+    this.timerSubscription = interval(1000)
+      .pipe(take(5)) // Take 5 emissions (5 seconds)
+      .subscribe({
+        next: () => {
+          this.countdown--;
+        },
+        complete: () => {
+          this.router.navigate(['/dashboard']);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    // Clean up subscription when component is destroyed
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
   }
 }

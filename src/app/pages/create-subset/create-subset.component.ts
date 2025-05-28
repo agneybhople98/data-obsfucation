1b06345d4b060c9b3c7934bd5a8f6f762ca5d3ac
Agney Arun Bhople <agney@Agneys-MacBook-Pro.local>
2025-05-29 01:35:24 +0530

    removed ci per from the table

--- src/app/pages/create-subset/create-subset.component.ts
+++ src/app/pages/create-subset/create-subset.component.ts
@@ -81,7 +81,7 @@ export class CreateSubsetComponent implements OnInit {
     'subsetRules',
   ];
 
-  tableItemsHealthcare = ['CI_ACCT', 'CI_PER'];
+  tableItemsHealthcare = ['CI_ACCT'];
 
   // 'CI_PER_NAME',
   // 'CI_PER_PHONE',
@@ -89,7 +89,7 @@ export class CreateSubsetComponent implements OnInit {
   // 'C1_ADDRESS',
   // 'CI_PER_ID',
   // 'CI_PER_CHAR',
-  tableItemsUtility = ['CI_ACCT', 'CI_PER'];
+  tableItemsUtility = ['CI_ACCT'];
 
   // 'CI_PER_NAME',
   //   'CI_PER_ADDR_SEAS',
